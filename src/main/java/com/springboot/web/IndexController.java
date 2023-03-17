1ae674d5e3ca5b17c8ba952b7bf3db09dc5a77a4
SeokNamLee <hhhhggggg@naver.com>
2023-03-17 16:05:49 +0900

    OAuth 2.0 로그인 기능 구현

--- src/main/java/com/springboot/web/IndexController.java
+++ src/main/java/com/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.springboot.web;
 
+import com.springboot.config.auth.LoginUser;
+import com.springboot.config.auth.dto.SessionUser;
 import com.springboot.service.PostsService;
 import com.springboot.web.DTO.PostsResponseDto;
 import lombok.Getter;
@@ -9,15 +11,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
-
     @GetMapping("/")
-    public String index(Model model)
+    public String index(Model model, @LoginUser SessionUser user)
     {
         model.addAttribute("posts",postsService.findAllDesc());
+
+        if (user != null)
+        {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -25,6 +33,7 @@ public class IndexController {
     public String postsSave() {
         return "posts-save";
     }
+
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model)
     {
