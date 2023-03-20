7e931686b37d0df03eb5a41ca12a620f500cea90
SeokNamLee <hhhhggggg@naver.com>
2023-03-20 18:03:52 +0900

    배포를 위한 appspec.yml 추가

--- src/main/java/com/springboot/Application.java
+++ src/main/java/com/springboot/Application.java
@@ -1,11 +1,14 @@
 package com.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
-public class Application{
-    public static void main(String[] args)
-    {
-        SpringApplication.run(Application.class, args);
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
+
     }
 }
